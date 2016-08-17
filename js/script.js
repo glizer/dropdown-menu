6037a8ce878084d3e596fd63c29f044fe2b7d6fd
Pavel Torbeev <p.torbeev@gmail.com>
2016-08-18 03:02:16 +0700

    Added disabling menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var dropdownBtn = document.getElementsByClassName('dropdown-btn'),
     priceBtns = document.querySelectorAll('.input-btn li'),
     inputs =  document.querySelectorAll('input'),
     OPEN_CLASS = 'open',
+    DISABLED_CLASS = 'disabled',
     DROPDOWN_MENU = '.dropdown-menu',
     DROPDOWN_CONTAINER = '.dropdown-container',
     priceFrom = 0,
@@ -25,7 +26,7 @@ for(var i = 0; i < dropdownBtn.length; i++) {
 
             parent.className = classes.join(' ');
         }
-        if (hasClass(parent)) {
+        if (hasClass(parent, OPEN_CLASS)) {
             setFocus(parent, 0);
         }
     }
@@ -46,26 +47,32 @@ for(var i = 0; i < priceBtns.length; i++) {
 }
 
 function setPrice(that,isParent) {
-    var priceParent = isParent ? that : that.parentElement,
+    var priceParent = isParent ? that.nextElementSibling : that.parentElement,
         id = isParent ? that.id : priceParent.getAttribute('data-for'),
         price = Number(isParent ? that.value : that.innerHTML);
 
-    var input = document.getElementById(id);
-    if (!(price > 0)) {
-        price = 0;
+    if (!hasClass(priceParent, DISABLED_CLASS)) {
+        var input = document.getElementById(id);
+        if (!(price > 0)) {
+            price = 0;
+        }
+        if (id === 'from') {
+            var focusParent = priceParent.closest(DROPDOWN_MENU);
+            setFocus(focusParent, 1);
+            priceFrom = price;
+            document.querySelector('[data-for="from"]').classList.add(DISABLED_CLASS);
+            document.querySelector('[data-for="to"]').classList.remove(DISABLED_CLASS);
+        } else {
+            price = price < priceFrom ? priceFrom : price;
+            var menu = priceParent.closest(DROPDOWN_CONTAINER);
+            close(menu);
+            priceTo = price;
+            document.querySelector('[data-for="to"]').classList.add(DISABLED_CLASS);
+            document.querySelector('[data-for="from"]').classList.remove(DISABLED_CLASS);
+        }
+        input.value = price;
+        setBtnTitle(priceFrom, priceTo)
     }
-    if (id === 'from') {
-        var focusParent = priceParent.closest(DROPDOWN_MENU);
-        setFocus(focusParent, 1);
-        priceFrom = price;
-    } else {
-        price = price < priceFrom ? priceFrom : price;
-        var menu = priceParent.closest(DROPDOWN_CONTAINER);
-        close(menu);
-        priceTo = price;
-    }
-    input.value = price;
-    setBtnTitle(priceFrom, priceTo)
 }
 
 function setBtnTitle(from, to) {
@@ -77,11 +84,11 @@ function setBtnTitle(from, to) {
 
 }
 
-function hasClass(element) {
+function hasClass(element, nameClass) {
     if (element.classList)
-        return element.classList.contains(OPEN_CLASS);
+        return element.classList.contains(nameClass);
     else
-        return new RegExp('(^| )' + OPEN_CLASS + '( |$)', 'gi').test(element.className);
+        return new RegExp('(^| )' + nameClass + '( |$)', 'gi').test(element.className);
 }
 
 function setFocus(element, number) {
